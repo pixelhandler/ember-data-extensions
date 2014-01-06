2a544d8f4f7ba5b0e1ceb558528d12e908b4d299
Bill Heaton <pixelhandler@gmail.com>
2014-01-06 00:41:06 -0800

    Documentation udpates following change to using only mixins

--- docs/api.js
+++ docs/api.js
@@ -1,20 +1,22 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "DS.EmbeddedJSONAdapter",
-        "DS.EmbeddedJSONMixin",
-        "DS.EmbeddedJSONSerializer"
+        "DS.EmbeddedAdapter",
+        "DS.EmbeddedMixin",
+        "DS.EmbeddedSerializer",
+        "DS.UnderscoredAdapterMixin",
+        "DS.UnderscoredSerializer"
     ],
     "modules": [
-        "embedded-json-adapter",
+        "embedded-adapter",
         "ember-data",
         "mixins"
     ],
     "allModules": [
         {
-            "displayName": "embedded-json-adapter",
-            "name": "embedded-json-adapter",
-            "description": "The `EmbeddedJSONAdapter` is a subclass of the RESTAdapter.\n\nA fork of `activemodel-adapter` with support for embedded `hasMany` and `belongsTo`\nrecords embedded in JSON payloads, designed to work out of the box with the\n[active_model_serializers](http://github.com/rails-api/active_model_serializers) Ruby gem.\n\n[Mongoid](https://github.com/mongoid/mongoid) supports using `embeds_many` and `embeds_one`\nin (Rails) models. Also `has_one` and `has_many` can be used with\n`ActiveModel::Serializers`. Choose an option for embedding ids or object(s).\n\nThis adapter extends the DS.RESTAdapter by making consistent use of the camelization,\ndecamelization and pluralization methods to normalize the serialized JSON into a\nformat that is compatible with a conventional Rails backend and Ember Data.\n\n## JSON Structure\n\nThe EmbeddedJSONAdapter expects the JSON payload from your server to follow\nthe REST adapter conventions substituting underscored keys for camelcased ones.\n\n### Conventional Names\n\nAttribute names in your JSON payload should be the underscored versions of\nthe attributes in your Ember.js models.\n\nFor example, if you have a `Person` model:\n\n```js\nApp.FamousPerson = DS.Model.extend({\n  firstName: DS.attr('string'),\n  lastName: DS.attr('string'),\n  occupation: DS.attr('string')\n});\n```\n\nThe JSON returned should look like this:\n\n```js\n{\n  \"famous_person\": {\n    \"first_name\": \"Barack\",\n    \"last_name\": \"Obama\",\n    \"occupation\": \"President\"\n  }\n}\n```"
+            "displayName": "embedded-adapter",
+            "name": "embedded-adapter",
+            "description": "DS.EmbeddedAdapter extends the DS.RESTSerializer adding mixin:\nDS.UnderscoredAdapterMixin"
         },
         {
             "displayName": "ember-data",
@@ -23,7 +25,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "mixins",
             "name": "mixins",
-            "description": "The EmbeddedJSONMixin allows you to add embedded record support to your serializers.\n\nTo set up embedded records, you include the mixin into the serializer and then\ndefine your embedded relations. The EmbeddedJSONSerializer is an example.\n\nBelow is an example of a per type serializer (post type).\n\n```js\nApp.PostSerializer = DS.RESTSerializer.extend(DS.EmbeddedJSONMixin, {\n  attrs: {\n    author: {embedded: 'always'},\n    comments: {embedded: 'always'}\n  }\n})\n```\n\nCurrently only `{embedded: 'always'}` records are supported."
+            "description": "DS.EmbeddedMixin supports serializing embedded records.\n\nTo set up embedded records, include the mixin into a serializer then\ndefine embedded (model) relationships.\n\nBelow is an example of a per type serializer (post type).\n\n```js\nApp.PostSerializer = DS.RESTSerializer.extend(DS.EmbeddedMixin, {\n  attrs: {\n    author: {embedded: 'always'},\n    comments: {embedded: 'always'}\n  }\n})\n```\n\nCurrently only `{embedded: 'always'}` records are supported."
         }
     ]
 } };

4418bf5f7c09067a882ce17905725340f1972862
Bill Heaton <pixelhandler@gmail.com>
2014-02-04 16:00:55 -0800

    update docs after dirty tracking mixins added

--- docs/api.js
+++ docs/api.js
@@ -2,8 +2,12 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "DS.EmbeddedAdapter",
+        "DS.EmbeddedInModel",
+        "DS.EmbeddedInModelMixin",
         "DS.EmbeddedMixin",
         "DS.EmbeddedSerializer",
+        "DS.ModelWithEmbedded",
+        "DS.ModelWithEmbeddedMixin",
         "DS.UnderscoredAdapterMixin",
         "DS.UnderscoredSerializer"
     ],
@@ -25,7 +29,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "mixins",
             "name": "mixins",
-            "description": "DS.EmbeddedMixin supports serializing embedded records.\n\nTo set up embedded records, include the mixin into a serializer then\ndefine embedded (model) relationships.\n\nBelow is an example of a per type serializer (post type).\n\n```js\nApp.PostSerializer = DS.RESTSerializer.extend(DS.EmbeddedMixin, {\n  attrs: {\n    author: {embedded: 'always'},\n    comments: {embedded: 'always'}\n  }\n})\n```\n\nCurrently only `{embedded: 'always'}` records are supported."
+            "description": "DS.EmbeddedInModelMixin"
         }
     ]
 } };
